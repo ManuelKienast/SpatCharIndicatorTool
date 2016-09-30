fd589f9a19c726b1de1e229513ccf1e134879975
niel_sm <niel_sm@dc92036e-722d-4c5d-96d9-57df35908678>
2016-09-30 10:21:19 +0000

    scripts included

---
+++ function_hex_grid.R
@@ -0,0 +1,195 @@
+#--------------------------------------------hex-grid function-----------------------------------------------------------------
+
+#this function creates a hex:grid fitted to the extent of a layer chosen by the user. User specifies the reference layer(table) 
+#as well as the desired cellwidth
+# The hex_grid will be fitted to the extent of the reference layer and the get the same SRID as the reference layer
+
+#' @param con : A connection to the PostGIS database.
+#' @param hex_width: As numeric - Width of grid cells
+#' @param schema1: As string - Name of the Schema in which the reference layer(table) is located
+#' @param table: As string - Reference layer over which the grid shall be cast
+#' @param schema2: As string - Name of the Schema where the fishnet shall be created
+#' @param name: As string - Name of the hex_grid to be created
+
+
+#-----------------------------------------------------------------------------------------------------------------------------
+
+
+setwd("X:\\DLR\\Daten")
+library(rgdal)
+library(RPostgreSQL)
+
+#creates connection to the database 
+
+
+
+
+hexgrid <- function(con, hex_width, schema1, table, schema2, name) {
+  
+  tableS = paste(schema1, table, sep = ".")
+  nameS = paste(schema2, name, sep = ".")
+  
+  clear = dbGetQuery(con, sprintf("DROP TABLE IF EXISTS %s;", nameS))
+  
+  get_SRID = dbGetQuery(con, sprintf("SELECT FIND_SRID('%s', '%s', 'the_geom');", schema1, table))
+  
+  xmin = dbGetQuery(con, sprintf("select (st_xmin(st_extent(%s.the_geom)) - (0.5*%s)) FROM %s;", tableS, hex_width, tableS)) 
+  
+  ymin = dbGetQuery(con, sprintf("select (st_ymin(st_extent(%s.the_geom)) - (0.5*%s)) FROM %s;", tableS, hex_width, tableS)) 
+  
+  xmax = dbGetQuery(con, sprintf("select (st_xmax(st_extent(%s.the_geom)) + (0.5*%s)) FROM %s;", tableS, hex_width, tableS)) 
+  
+  ymax = dbGetQuery(con, sprintf("select (st_ymax(st_extent(%s.the_geom)) + (0.5*%s)) FROM %s;", tableS, hex_width, tableS)) 
+  
+  
+  create_hexgrid = dbGetQuery(con, sprintf("CREATE TABLE %s (gid serial not null primary key);
+                                           
+                                           SELECT addgeometrycolumn('%s', '%s','the_geom', 0, 'POLYGON', 2);
+                                           
+                                           CREATE OR REPLACE FUNCTION genhexagons(width float, xmin float, ymin  float, xmax float, ymax float  )
+                                           
+                                           RETURNS float AS $total$
+                                           declare
+                                           
+                                           b float :=width/2;
+                                           a float :=b/2; --sin(30)=.5
+                                           c float :=2*a;
+                                           
+                                           height float := 4*a+c;  --1.1547*width;
+                                           ncol float :=ceil(abs(xmax-xmin)/width);
+                                           nrow float :=ceil(abs(ymax-ymin)/height);
+                                           
+                                           polygon_string varchar := 'POLYGON((' ||
+                                           0 || ' ' || 0     || ' , ' ||
+                                           b || ' ' || a     || ' , ' ||
+                                           b || ' ' || a+c   || ' , ' ||
+                                           0 || ' ' || a+c+a || ' , ' ||
+                                           -1*b || ' ' || a+c   || ' , ' ||
+                                           -1*b || ' ' || a     || ' , ' ||
+                                           0 || ' ' || 0     ||
+                                           '))';
+                                           
+                                           BEGIN
+                                           INSERT INTO %s (the_geom) SELECT 
+                                           st_translate(the_geom, x_series*(2*a+c)+xmin, y_series*(2*(c+a))+ymin)
+                                           
+                                           from generate_series(0, ncol::int , 1) as x_series,
+                                           generate_series(0, nrow::int, 1 ) as y_series,
+                                           (SELECT polygon_string::geometry as the_geom
+                                           UNION
+                                           SELECT ST_Translate(polygon_string::geometry, b , a+c)  as the_geom
+                                           ) as two_hex;
+                                           
+                                           
+                                           
+                                           ALTER TABLE %s
+                                           ALTER COLUMN the_geom TYPE geometry(Polygon, %s)
+                                           USING ST_SetSRID(the_geom, %s);
+                                           RETURN NULL;
+                                           END;
+                                           $total$ LANGUAGE plpgsql;
+                                           
+                                           SELECT genhexagons(%s,%s,%s,%s,%s);", nameS, schema2, name, nameS, nameS, get_SRID, get_SRID, hex_width, xmin, ymin, xmax, ymax)                   
+                              
+                              
+                              
+  )
+  
+}
+
+
+
+#----------------------------------------------------------------------------------------------------------------------
+
+#Usage:
+
+con <- dbConnect(dbDriver("PostgreSQL"),
+                 dbname = "urmo",
+                 host = "localhost",
+                 user = "postgres", 
+                 password = "postgres")
+
+
+
+hexgrid(con, 500, "urmo", "tvz", "public", "hex_grid_500")
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+
+#disconnect DB Connection
+dbDisconnect(con)
+
+
+#SQL-Queries:
+
+#hex_grid function:
+#
+# clear existing table: DROP TABLE IF EXISTS name;
+
+# get SRID of ex_area: SELECT FIND_SRID('public', 'table', 'geom');
+
+# get xmin (offset by: -hex_width/2) of bounding box  of ex_area : 
+# SELECT (st_xmin(st_extent(table.geom)) - (0.5*hex_width)) FROM table; 
+#
+# get ymin (offset by: -hex_width/2) of bounding box  of ex_area :
+# SELECT (st_ymin(st_extent(table.geom)) - (0.5*hex_width)) FROM table;
+#
+# get xmax (offset by: +hex_width/2) of bounding box  of ex_area :
+# SELECT (st_xmax(st_extent(table.geom)) + (0.5*hex_width)) FROM table;
+#
+# get ymax (offset by: +hex_width/2) of bounding box  of ex_area :
+# SELECT (st_ymax(st_extent(table.geom)) + (0.5*hex_width)) FROM table;
+# 
+# create and call fct hex_grid:
+
+# CREATE TABLE hex_grid (gid serial not null primary key);
+
+# SELECT addgeometrycolumn('hex_grid','geom', 0, 'POLYGON', 2);
+#
+# CREATE OR REPLACE FUNCTION genhexagons(width float, xmin float, ymin  float, xmax float, ymax float  )
+
+# RETURNS float AS $total$
+#  declare
+
+# b float :=width/2;
+# a float :=b/2; --sin(30)=.5
+# c float :=2*a;
+
+# height float := 4*a+c;  --1.1547*width;
+# ncol float :=ceil(abs(xmax-xmin)/width);
+# nrow float :=ceil(abs(ymax-ymin)/height);
+
+# polygon_string varchar := 'POLYGON((' ||
+#   0 || ' ' || 0     || ' , ' ||
+#   b || ' ' || a     || ' , ' ||
+#   b || ' ' || a+c   || ' , ' ||
+#   0 || ' ' || a+c+a || ' , ' ||
+#   -1*b || ' ' || a+c   || ' , ' ||
+#   -1*b || ' ' || a     || ' , ' ||
+#   0 || ' ' || 0     ||
+#   '))';
+
+# BEGIN
+# INSERT INTO hex_grid (geom) SELECT 
+# st_translate(geom, x_series*(2*a+c)+xmin, y_series*(2*(c+a))+ymin)
+
+# from generate_series(0, ncol::int , 1) as x_series,
+# generate_series(0, nrow::int, 1 ) as y_series,
+# (SELECT polygon_string::geometry as geom
+#  UNION
+#  SELECT ST_Translate(polygon_string::geometry, b , a+c)  as geom
+# ) as two_hex;
+#
+# ALTER TABLE hex_grid RENAME TO name;
+#
+# ALTER TABLE name
+# ALTER COLUMN geom TYPE geometry(Polygon, get_SRID)
+# USING ST_SetSRID(geom, get_SRID);
+# RETURN NULL;
+# END;
+# $total$ LANGUAGE plpgsql;
+
+# SELECT genhexagons(hex_width, xmin, ymin, xmax, ymax)                   
+
---
+++ function_landuse.R
@@ -0,0 +1,111 @@
+# Christina am 06.09.2016
+# Berechnung von  Flaechenanteilen pro Aggregationszelle
+
+
+
+#' Spatial Indicator for a proportion of areas.
+#' Builds a proportion of areas of a selected category per aggregation cell.
+#' Note, that splitted areas will be kept and calculated to the aggregation unit it intersects with. 
+#' 
+#' @param con A connection to the PostGIS database.
+#' @param schema A string: the name of the scheme in the database, in which the tables are included.
+#' @param table A string: the name of the table in the PostGIS database containing the geometry you want to know the ratio.
+#' @param category A string: the name of the column of the categories.
+#' @param aggregation A string: the name of the table in the PostGIS database containing the aggregation units.
+#' @param aggr_id A string: the name of the identifier of the aggregation units.
+#' @param intersection A string: the name of the new table containing the intersected areas.
+#' 
+#' @return area A data.frame containing the aggretaion units, their categories (use), the area of that category and the proportion of that area to the area of the aggregation unit in percent. 
+#' 
+#' @examples 
+#' areaRatio(con, "public", "landuse", "landuse", "TVZ", "VBZ_NO")
+#'            aggr_unit           landuse      area area_ratio
+#'        1   110100111        commercial  46508.39       6.22
+#'        2   110100111      construction   1035.61       0.14
+#'        3   110100111             grass   5049.22       0.68
+
+
+
+setwd("d:\\Projekt\\")
+#install.packages("L:\\Projekt\\Packages\\RPostgreSQL_0.4-1.zip",repos = NULL) 
+#install.packages("L:\\Projekt\\Packages\\DBI_0.5.zip", repos = NULL)
+library(RPostgreSQL)
+
+## Connect to database
+drv <- dbDriver("PostgreSQL")
+con <- dbConnect(drv, dbname = "dlr2", host = "localhost", user = "postgres", password = "postgres") 
+dbListTables(con)
+
+
+#Gives the proportion of areas of a selected category per aggregation cell.
+#Note, that splitted areas will be kept and calculated to the aggregation unit it intersects with. 
+
+areaRatio <- function(con, schema = "public", table, category, aggr_schema = "public", aggregation, aggr_id, out_schema = "public", output, intersection = "intersection") {
+  #creates a table intersecting the input data with aggregation cells
+  #erstellt eine Tabelle die die überschneiden Flächen von Landuse mit den Flächen der TVZ je TVZ ausgibt
+  interSection <- dbGetQuery(con, sprintf(
+    "DROP TABLE IF EXISTS %s;
+      CREATE TABLE %s (gid serial PRIMARY KEY, aggr_unit integer, use varchar);
+      ALTER TABLE %s ADD COLUMN geom geometry (MultiPolygon, 25833);
+
+      INSERT INTO %s (
+      SELECT row_number() over (order by 1) as gid, b.%s::numeric, a.%s, ST_Multi(ST_Intersection(a.geom, b.geom))::geometry(MultiPolygon, 25833) as geom
+      FROM %s.%s a, %s.%s b WHERE ST_Intersects(a.geom, b.geom));
+      ", intersection, intersection, intersection, intersection, aggr_id, category, schema, table, aggr_schema, aggregation))
+
+  #calculates the part of area per aggregation unit 
+  #Berechnet den Flächenanteil pro landuse-Kategorie in jeder TVZ
+    ratio <- dbGetQuery(con, sprintf(
+      "DROP TABLE IF EXISTS %s.%s;
+      CREATE TABLE %s.%s (gid serial PRIMARY KEY, aggr_unit varchar, category varchar, area numeric, ratio numeric); 
+      INSERT INTO %s.%s (
+      SELECT row_number() over (order by 1) as gid, a.aggr_unit, a.use, round(sum(ST_AREA(a.geom))::numeric, 2) as area, round((100/ (ST_AREA(b.geom))*sum(ST_AREA(a.geom)))::numeric,2) as area_ratio 
+      FROM public.%s as a LEFT JOIN %s.%s as b ON a.aggr_unit = b.%s::numeric
+      GROUP BY a.aggr_unit, a.use, b.geom
+      ORDER BY a.aggr_unit);
+      ", out_schema, output, out_schema, output, out_schema, output, intersection, aggr_schema, aggregation, aggr_id)) 
+    delete <- dbGetQuery(con, "DROP TABLE intersection")
+    ratio
+    
+} 
+
+
+
+#----------------------------------------------------------------------------------------------------------------------
+
+#Usage:
+
+#areaRatio(con, "public", "landuse", "landuse", "public", "tvz", "vbz_no", "public", "arearatio")
+
+#areaRatio(con, table = "landuse", category = "landuse", aggregation = "vkz", aggr_id = "visum_no", output = "arearatio")
+
+#areaRatio(con, "public", table = "landuse", category = "landuse",  aggregation = "rbs_od_blk_2015_mitte", aggr_id = "blk", output = "arearatio")
+
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+#disconnect DB Connection
+dbDisconnect(con)
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+#SQL-Query:
+
+#DROP TABLE IF EXISTS intersection;
+#CREATE TABLE intersection (gid serial PRIMARY KEY, aggr_unit integer, use varchar);
+#ALTER TABLE intersection ADD COLUMN geom geometry (MultiPolygon, 25833);
+
+#INSERT INTO intersection (SELECT row_number() over (order by 1) as gid, b.aggr_id, a.category, ST_Multi(ST_Intersection(a.geom, b.geom))::geometry(MultiPolygon, 25833) as geom
+#FROM schema.table a, aggr_schema.aggregation b WHERE ST_Intersects(a.geom, b.geom));
+
+#DROP TABLE IF EXISTS out_schema.output;
+#CREATE TABLE out_schema.output (gid serial PRIMARY KEY, aggr_unit varchar, category varchar, area numeric, ratio numeric); 
+#INSERT INTO out_schema.output (
+#SELECT row_number() over (order by 1) as gid, a.aggr_unit, a.use, round(sum(ST_AREA(a.geom))::numeric, 2) as area, round((100/ (ST_AREA(b.geom))*sum(ST_AREA(a.geom)))::numeric,2) as area_ratio 
+#FROM public.intersection as a LEFT JOIN aggr_schema.aggregation as b ON a.aggr_unit = b.aggr_id 
+#GROUP BY a.aggr_unit, a.use, b.geom
+#ORDER BY a.aggr_unit;
+
+#DROP TABLE intersection;
+
