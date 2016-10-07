f95e7615a5f7f114c3add4c404b2b4cd82859bb1
kien_ma <kien_ma@dc92036e-722d-4c5d-96d9-57df35908678>
2016-10-07 13:16:15 +0000

    ## for calling as scource

--- function_categorize.R
+++ function_categorize.R
@@ -5,19 +5,19 @@
 
 
 
-library(RPostgreSQL)
-library(rgdal)
-
-
-#creates connection to the database 
-con <- dbConnect(dbDriver(  "PostgreSQL"),
-                 dbname =   "dlr",
-                 host =     "localhost",
-                 user =     "postgres", 
-                 password = "postgres")
-
-#lists all tables of chosen database
-dbListTables(con)
+# library(RPostgreSQL)
+# library(rgdal)
+# 
+# 
+# #creates connection to the database 
+# con <- dbConnect(dbDriver(  "PostgreSQL"),
+#                  dbname =   "dlr",
+#                  host =     "localhost",
+#                  user =     "postgres", 
+#                  password = "postgres")
+# 
+# #lists all tables of chosen database
+# dbListTables(con)
 
 #----------------------------PARAMETERS-------------------------------------------------------------------------------------------
 
@@ -116,16 +116,16 @@ categorize <- function(connection, tableAggregation, AggrField, tableCategory, c
 
 #function(connection, tableAggregation, AggrField, tableCategory, crit1Field, crit2Field, crit1LV1, crit1LV2, crit1LV3, crit2Sep, output1, output2)
 
-categorize(con, "tvz","vbz_no", "mietobjekte", "anzahletag", "baujahr", 1, 2, 6, 1948, "categories_tvz", "cat_tvz_count") 
-
-categorize(con, "hex_mietobjekte","gid", "mietobjekte", "anzahletag", "baujahr", 1, 2, 6, 1948, "categories_tvz", "cat_tvz_count")
-
-
-#---------------------------------------------------------------------------------------------------------------------------------------------
-
-
-#disconnect DB Connection
-dbDisconnect(con)
+# categorize(con, "tvz","vbz_no", "mietobjekte", "anzahletag", "baujahr", 1, 2, 6, 1948, "categories_tvz", "cat_tvz_count") 
+# 
+# categorize(con, "hex_mietobjekte","gid", "mietobjekte", "anzahletag", "baujahr", 1, 2, 6, 1948, "categories_tvz", "cat_tvz_count")
+# 
+# 
+# #---------------------------------------------------------------------------------------------------------------------------------------------
+# 
+# 
+# #disconnect DB Connection
+# dbDisconnect(con)
 
 
 
