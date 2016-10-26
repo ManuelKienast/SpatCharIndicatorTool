07750533ba6e2869c96b31145141519b9b8a2fe2
Kienast <Manuel.Kienast@dlr.de>
2016-10-26 17:31:05 +0200

    first steps

---
+++ indicator/calcPop_viaKGS44.R
@@ -0,0 +1,42 @@
+## Population BreakDown
+## From PLR to GridCell 
+## following calculate_various_rates_per_plr_from_srv.sql - script
+## disaggregating the Block-population onto the no of households, and then summing those households to Grid cell
+
+
+calcPop2Cell <- function( connection, )
+  
+{
+  popPerCell <- dbGetQuery(connection, sprintf( 
+  
+  "  WITH pop_hh AS      -- which plr is each household located in?
+(
+  SELECT 	p.blk_id, 
+  p.pop_tot/sum(k.hh) As PopPerHh,
+  k.kgs44_id
+  FROM urmo.pop_blk p 
+  JOIN urmo.kgs44 k ON ST_Within(k.the_geom, p.the_geom)
+  WHERE k.hh > 0
+  GROUP BY p.blk_id, p.pop_tot, k.kgs44_id
+  ),
+  
+  hh_Grid AS             -- which Aggregation cell (GridCell) is each household located in?
+  (
+  SELECT s.gid, k.kgs44_id, k.hh
+  FROM urmo.kgs44 k 
+  JOIN grids.hex_2000 s ON ST_Within(k.the_geom, s.the_geom)
+  GROUP BY s.gid, k.kgs44_id, k.hh
+  )
+  SELECT 
+  hh_G.gid,
+  sum(pop.PopPerHh)	
+  FROM pop_hh AS pop
+  join hh_Grid AS hh_G
+  ON (pop.kgs44_id = hh_G.kgs44_id)
+  GROUP BY hh_G.gid
+  ORDER BY hh_G.gid
+
+;"
+,
+))
+}
