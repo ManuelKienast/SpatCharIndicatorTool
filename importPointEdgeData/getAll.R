606cb6bef4ed735c948d9484628945ba39f04450
Kienast <Manuel.Kienast@dlr.de>
2016-11-01 16:31:13 +0100

    Version 1.0 - working

--- importPointEdgeData/getAll.R
+++ importPointEdgeData/getAll.R
@@ -2,7 +2,9 @@
 ## Import Script for 'aggregated_oneshot_meso.csv' data
 ##
 
-source(\\importPOintEdgeData)
+source("importPOintEdgeData\\importPlainNodCSV.r")
+source("importPOintEdgeData\\importPlainEdgeCSV.r")
+source("importPOintEdgeData\\importaggregatedOneShotCSV.r")
 
 
 getAll2import <- function( con,
@@ -23,18 +25,29 @@ getAll2import <- function( con,
   
 {
   
-plainNod<-  importPlainNodCSV( connection,
+plainNod<-  importPlainNodCSV( con,
                                table_schema, table_name_nod,
                                set_CS2, filepath_nod)
 
-plainEdg <- importPlainEdgCSV( connection,
+plainEdg <- importPlainEdgCSV( con,
                                table_schema, table_name_edg,
                                set_CS2, filepath_edg)
       
-aggregate <-  importaggregatedOneShotCSV( connection,
+aggregate <-  importaggregatedOneShotCSV( con,
                                           table_schema, table_name_aggregated,
                                          filepath_aggregated)
   
   }
   
+
+USAGE:
+  getAll2import( con,
+                             "public",
+                             "test_nod",
+                             "D:\\Manuel\\vonSimon\\Von Matthias\\net_plain.nod.csv",
+                             "test_edg",
+                             "D:\\Manuel\\vonSimon\\Von Matthias\\net_plain.edg.csv",
+                             "test_aggregated",
+                             "D:\\Manuel\\vonSimon\\Von Matthias\\aggregated_oneshot_meso.csv",
+                             "25833")
       
