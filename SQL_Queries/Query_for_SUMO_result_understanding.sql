fbb5756f814411f71fa9ab552a9912af09cf70c7
Kienast <Manuel.Kienast@dlr.de>
2016-11-02 16:23:41 +0100

    -import Updates

---
+++ SQL_Queries/Query_for_SUMO_result_understanding.sql
@@ -0,0 +1,82 @@
+﻿SELECT *
+FROM test_aggregated
+limit 100;
+
+--- presentation of trip travel time intervals
+SELECT DISTINCT interval_begin, AVG(interval_end), (AVG(interval_end)-interval_begin) As delta_t
+FROM test_aggregated
+GROUP BY interval_begin
+Order BY interval_begin;
+
+--- count of trips per interval, following an expected daily distribution, plateu from 11:30 to 16:00 peak near 14:00
+SELECT DISTINCT interval_begin, COUNT(gid)
+FROM test_aggregated
+GROUP BY interval_begin
+Order BY interval_begin;
+
+--- test for uniqueness of edge_id: definetly NOT, but could be a measuere of the intensity of travel per edge_id
+SELECT edge_id, COUNT(*)
+FROM test_aggregated
+GROUP BY edge_id
+HAVING COUNT(*) > 1
+
+--- but testing for its uniqueness order by time intervals reveals uniquness!
+SELECT interval_begin, edge_id, COUNT(*)
+FROM test_aggregated
+GROUP BY interval_begin, edge_id
+HAVING COUNT(*) > 1
+
+--- checking for differences btwn edge_sampleseconds and edge_travel_time --> whatever, nothing conclusive, just generally they are not equal; 350k rows are; -> random
+SELECT edge_sampledseconds, edge_traveltime
+FROM test_aggregated
+WHERE edge_sampledseconds = edge_traveltime
+
+--- check edge_density
+SELECT DISTINCT edge_density, edge_occupancy
+FROM test_aggregated
+Order BY edge_density desc;
+
+--- cross check with test_Edge (geom) data with no geom column, ie.e edge_id -53424914
+select *
+FROM test_edg
+LIMIT 1
+
+select *
+FROM test_edg
+where edge_shape IS NULL
+LIMIT 10
+
+select *
+FROM test_aggregated
+where edge_id IN (
+'-53499553',
+'-53499554',
+'-53499555',
+'-53499562',
+'-53499563',
+'-53499567',
+'-53499568',
+'-53499570',
+'-53499572',
+'-53499573',
+'-53424914')
+
+--- check the nodes, there are conflicts in the node_id col
+select *
+from test_nod
+limit 1
+
+select *
+from test_nod
+where node_id LIKE '%e' OR node_id LIKE '%.%'
+
+select DISTINCT node_Type
+from test_nod
+
+--- Join the whole thing ..
+SELECT a.interval_begin, a.edge_arrived, a.edge_departed, a.edge_entered, a.edge_left, e.edge_id, n.node_id, n.geom
+FROM test_Aggregated AS a
+LEFT JOIN test_edg AS e
+	ON (a.edge_id = e.edge_id)
+LEFT JOIN test_nod As n
+	ON(e.edge_to = n.node_id)
--- importPointEdgeData/getAll.R
+++ importPointEdgeData/getAll.R
@@ -1,10 +1,10 @@
 ##
 ## Import Script for 'aggregated_oneshot_meso.csv' data
+## and possibly compiling all 'relevant' information into one table (see function @bottom)
 ##
 
-source("importPOintEdgeData\\importPlainNodCSV.r")
-source("importPOintEdgeData\\importPlainEdgeCSV.r")
-source("importPOintEdgeData\\importaggregatedOneShotCSV.r")
+source("importPOintEdgeData\\getAll_functionCompilation.r")
+
 
 
 getAll2import <- function( con,
@@ -16,13 +16,6 @@ getAll2import <- function( con,
                             table_name_aggregated,
                             filepath_aggregated,
                             set_CS2)
-  
-  
-  
-  
-  
-
-  
 {
   
 plainNod<-  importPlainNodCSV( con,
@@ -50,4 +43,173 @@ aggregate <-  importaggregatedOneShotCSV( con,
                              "test_aggregated",
                              "D:\\Manuel\\vonSimon\\Von Matthias\\aggregated_oneshot_meso.csv",
                              "25833")
+
+    
+##
+# ########################################################################################################################################
+# ########################################################################################################################################
+##
+    
+  compile2Table <- function ()
+    
+  {
+    
+    new_table <- dbGetQuery(connection, sprintf(
       
+      "
+      DROP TABLE IF EXISTS public.compiledImport;
+      
+      SELECT * INTO public.compiledImport FROM
+        (SELECT 
+          a.interval_begin,
+          a.edge_arrived,
+          a.edge_departed,
+          a.edge_entered,
+          a.edge_left,
+          e.edge_id,
+          n.node_id,
+          n.geom
+        FROM test_Aggregated AS a
+          LEFT JOIN test_edg AS e
+            ON (a.edge_id = e.edge_id)
+          LEFT JOIN test_nod As n
+            ON(e.edge_to = n.node_id)
+                
+      ) as foo;
+      
+      ALTER TABLE public.compiledImport
+      ADD COLUMN gid SERIAL PRIMARY KEY;
+
+      CREATE INDEX compiledImport_gix 
+        ON public.compiledImport
+        USING GIST (geom);
+      "
+      ))
+}
+  
+  
+  ##
+  ##
+  # ########################################################################################################################################
+  # #
+  # # Raw SQL-Code
+  # 
+  # SELECT a.interval_begin, a.edge_arrived, a.edge_departed, a.edge_entered, a.edge_left, e.edge_id, n.node_id, n.geom
+  #   FROM test_Aggregated AS a
+  #     LEFT JOIN test_edg AS e
+  #       ON (a.edge_id = e.edge_id)
+  #     LEFT JOIN test_nod As n
+  #       ON(e.edge_to = n.node_id)
+  #     
+  
+  
+#   ##
+#   ##  (UN-)DEAD-END
+#   # ########################################################################################################################################  
+#   ## Create Table with a single Geom per point and not 68 like above
+#   
+#   
+#   ## get the vector fot time intervals
+#   
+#   getV_tint <- function (connection)
+#     
+#     {V_tintdf <- dbGetQuery(connection, sprintf(
+#       "SELECT DISTINCT interval_begin 
+#       FROM public.compiledImport
+#       ORDER BY interval_begin
+#   
+#   ;"))
+#   
+#   V_tint <- V_tintdf[,1]
+#   
+#   return(V_tint)    
+#   }
+#   
+#   V_tint <- getV_tint (con)
+#   
+#   
+#   ## Create base table
+#   
+#   createBaseTable <- function (connection)
+#     
+#     {
+#     baseTable <- dbGetQuery(connection, sprintf(
+#       
+#       "DROP TABLE IF EXISTS public.cImport;
+#       SELECT * INTO public.cImport
+#         FROM (SELECT DISTINCT
+#           node_id,
+#           geom,
+#           edge_id
+#             FROM public.compiledImport
+#               )as Foo;
+#       
+#       ALTER TABLE public.cImport
+#         ADD COLUMN gid SERIAL PRIMARY KEY;
+#       
+#       CREATE INDEX cImport_gix 
+#         ON public.cImport
+#       USING GIST (geom);
+#       "))
+#     }
+# 
+#   cImport <- createBaseTable(con)
+#   
+#   ### adding the loop to fill the cols with data acccording to the timeframe defined in the V_tint
+#   
+#   ###VColnames <- c("interval_begin", "edge_arrived", "edge_departed", "edge_entered", "edge_left", "edge_id", "node_id")
+#   
+#   
+#   
+#   compile2SingleGeomTable <- function (con, V_tint)  
+#    
+#   { 
+#     singlegeomTable <- dbGetQuery(connection, sprintf(
+#     "
+#   ALTER TABLE public.cImport DROP COLUMN IF EXISTS interval_begin_%s;
+#   ALTER TABLE public.cImport ADD COLUMN interval_begin_%s FLOAT;
+# 
+#   ALTER TABLE public.cImport DROP COLUMN IF EXISTS edge_arrived_%s;
+#   ALTER TABLE public.cImport ADD COLUMN edge_arrived_%s FLOAT;
+# 
+#   ALTER TABLE public.cImport DROP COLUMN IF EXISTS edge_departed_%s;
+#   ALTER TABLE public.cImport ADD COLUMN edge_departed_%s FLOAT;
+# 
+#   ALTER TABLE public.cImport DROP COLUMN IF EXISTS edge_entered_%s;
+#   ALTER TABLE public.cImport ADD COLUMN edge_entered_%s FLOAT;
+# 
+#   ALTER TABLE public.cImport DROP COLUMN IF EXISTS edge_left_%s;
+#   ALTER TABLE public.cImport ADD COLUMN edge_left_%s FLOAT;
+#   "
+#   ,
+#    V_tint,V_tint,
+#     V_tint,V_tint,
+#     V_tint,V_tint,
+#     V_tint,V_tint,
+#     V_tint,V_tint))
+# 
+# }
+# 
+# for ( i in V_tint ) compile2SingleGeomTable (con, i)
+# 
+#     UPDATE public.cImport
+#       SET interval_begin_%s = c.interval_begin,
+#       SET edge_arrived_%s   = c.edge_arrived,
+#       SET edge_departed_%s  = c.edge_departed,
+#       SET edge_entered_%s   = c.edge_entered,
+#       SET edge_left_%s      = c.edge_left
+#         FROM public.cIMport 
+#           LEFT JOIN public.compiledImport AS c
+#            ON node_id = c.node_id
+#         WHERE c.interval_begin = '%s' AND node_id = c.node_id
+#     ;"
+#  
+#   
+#    V_tint,V_tint,V_tint,V_tint,V_tint,
+#   
+#    V_tint
+# 
+#   ))
+#   }
+#   
+#   for ( i in V_tint ) compile2SingleGeomTable (con, i)
