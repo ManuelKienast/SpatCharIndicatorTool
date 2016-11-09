304e6ec95207e5bc085e2c50227f110a6a1c08d1
Kienast <Manuel.Kienast@dlr.de>
2016-11-09 15:52:20 +0100

    - loop for fish net & equal area calcs

--- grid/calculate_grids.R
+++ grid/calculate_grids.R
@@ -9,6 +9,14 @@ con <- dbConnect(dbDriver("PostgreSQL"),
                         dbname = "urmo")
 
 
+## Since grids computed with the same width are not equal in area (grid_fish A=w*w, yes those are squares;)|(grid_hex A=w*3/4w)
+## these two vectors are proposed for nearly equal-area grids, with 
+## area measurements of c(0.125 km², 0.5 km², 2 km², 8 km²)
+## for hex_grids:       c(408.25, 816.5, 1633, 3266)
+## for fish_grids:      C(353.55, 707.22, 1414.21, 2828.43)
+
+
+
 ## Gridsizes in meter
 grids <- c(8000, 4000, 2000, 1000, 500)
 
