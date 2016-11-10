59c315244d493072f555449b6ce41ac8f41b0a0f
Kienast <Manuel.Kienast@dlr.de>
2016-11-10 16:01:47 +0100

    - update

--- indicator/qntfyLines_forCleanOSM_Bike.R
+++ indicator/qntfyLines_forCleanOSM_Bike.R
@@ -32,33 +32,33 @@ createInterSecTable <- function (
     "DROP TABLE IF EXISTS InterSec;
     
     SELECT * INTO public.InterSec FROM
-    (SELECT 
-    row_number() over (order by 1) as key,
-    Agg_Area.%s AS Agg_ID,
-    Ex_Area.%s AS LineType,
-    --Ex_Area.vmax AS speed,	
-    ST_Multi(ST_Intersection(Agg_Area.%s, ST_Transform(Ex_Area.%s, 25833)))::geometry(multiLineString, 25833) as geom
-    FROM
-    %s AS Agg_Area
-    LEFT JOIN %s AS Ex_Area
-    ON (ST_INTERSECTS(Agg_Area.%s, ST_Transform(Ex_Area.%s, 25833)))
-    WHERE 
-    -- Ex_Area.%s LIKE '%s' AND 
-    ST_isValid(Agg_Area.%s) = TRUE AND ST_isValid(ST_Transform(Ex_Area.%s, 25833)) = TRUE 
-    ) as foo
-    ;
-    ALTER TABLE InterSec ADD PRIMARY KEY (key);"
+      (SELECT 
+        row_number() over (order by 1) as key,
+        Agg_Area.%s AS Agg_ID,
+        Ex_Area.%s AS LineType,
+        ST_Multi(ST_Intersection(Agg_Area.%s, ST_Transform(Ex_Area.%s, 25833)))::geometry(multiLineString, 25833) as geom
+          FROM
+            %s AS Agg_Area
+              LEFT JOIN %s AS Ex_Area
+            ON (ST_INTERSECTS(Agg_Area.%s, ST_Transform(Ex_Area.%s, 25833)))
+              WHERE 
+                  -- Ex_Area.%s LIKE '%s' AND 
+                  ST_isValid(Agg_Area.%s) = TRUE AND ST_isValid(ST_Transform(Ex_Area.%s, 25833)) = TRUE 
+      ) as foo;
+
+    ALTER TABLE InterSec ADD PRIMARY KEY (key)
+    ;"
     ,
-    id_column,                    ## Agg_Area   -- column with the unique Agg_Area_ID e.g. PLR-id
+    id_column,                    ## Agg_Area         -- column with the unique Agg_Area_ID e.g. PLR-id
     label_column,                 ## label_column.    -- column with linetype specification
-    ## Ex_speed,                  -- column holding the max speed per line type, or any secondary objects
-    Agg_geom, Ex_geom,            ## ST_Multi   -- geometry columns of both Agg and Ex objects
-    Agg_Area,                     ## FROM       -- table containing the Aggreation Area geometries 
-    Ex_Area,                      ## LEFT JOIN  -- table containing the Examination Object  geometries and information here: lineTypes
-    Agg_geom, Ex_geom,            ## ON         -- geometrie columns of both Agg and Ex objects
-    ## label_column, "highway%",  ## WHERE      -- type of Line and query for highway in its description --> its an OSM-special
-    Agg_geom, Ex_geom,             ## WHERE      -- geometrie columns of both Agg and Ex objects
-    Agg_geom, Ex_geom             ## WHERE      -- geometrie columns of both Agg and Ex objects
+    
+    Agg_geom, Ex_geom,            ## ST_Multi         -- geometry columns of both Agg and Ex objects
+    Agg_Area,                     ## FROM             -- table containing the Aggreation Area geometries 
+    Ex_Area,                      ## LEFT JOIN        -- table containing the Examination Object  geometries and information here: lineTypes
+    Agg_geom, Ex_geom,            ## ON               -- geometrie columns of both Agg and Ex objects
+    ## label_column, "highway%",  ## WHERE            -- type of Line and query for highway in its description --> its an OSM-special
+    Agg_geom, Ex_geom,            ## WHERE            -- geometrie columns of both Agg and Ex objects
+    Agg_geom, Ex_geom             ## WHERE            -- geometrie columns of both Agg and Ex objects
     
     ))
   
