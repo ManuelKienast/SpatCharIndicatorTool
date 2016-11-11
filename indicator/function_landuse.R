a6294986298142935a884d18a6a367a1871387a3
Kienast <Manuel.Kienast@dlr.de>
2016-11-11 17:00:32 +0100

    -- version 1.1 fixed with variable geom and new namespace

--- indicator/function_landuse.R
+++ indicator/function_landuse.R
@@ -7,15 +7,15 @@
 #' Builds a proportion of areas of a selected category per aggregation cell.
 #' Note, that splitted areas will be kept and calculated to the aggregation unit it intersects with. 
 #' 
-#' @param con A connection to the PostGIS database.
-#' @param schema A string: the name of the scheme in the database, in which the tables are included.
-#' @param table A string: the name of the table in the PostGIS database containing the geometry you want to know the ratio.
-#' @param category A string: the name of the column of the categories.
-#' @param aggregation A string: the name of the table in the PostGIS database containing the aggregation units.
-#' @param aggr_id A string: the name of the identifier of the aggregation units.
-#' @param intersection A string: the name of the new table containing the intersected areas.
+#' @param con            A connection to the PostGIS database.
+#' @param ex_table_schema         A string: the name of the scheme in the database, in which the tables are included.
+#' @param ex_table          A string: the name of the table in the PostGIS database containing the geometry you want to know the ratio.
+#' @param ex_table_id       A string: the name of the column of the categories.
+#' @param grid_table    A string: the name of the table in the PostGIS database containing the aggregation units.
+#' @param grid_id        A string: the name of the identifier of the aggregation units.
+#' @param intersec_table_name   A string: the name of the new table containing the intersected areas.
 #' 
-#' @return area A data.frame containing the aggretaion units, their categories (use), the area of that category and the proportion of that area to the area of the aggregation unit in percent. 
+#' @return area A        data.frame containing the aggretaion units, their categories (use), the area of that category and the proportion of that area to the area of the aggregation unit in percent. 
 #' 
 #' @examples 
 #' areaRatio(con, "public", "landuse", "landuse", "TVZ", "VBZ_NO")
@@ -40,48 +40,127 @@
 #Gives the proportion of areas of a selected category per aggregation cell.
 #Note, that splitted areas will be kept and calculated to the aggregation unit it intersects with. 
 
-areaRatio <- function(con, schema = "public", table, category, aggr_schema = "public", aggregation, aggr_id, out_schema = "public", output, intersection = "intersection") {
+calcAreaRatio <- function( con,
+                       ex_table_schema = "public", ex_table, ex_table_id, ex_table_geom,
+                       grid_schema = "public", grid_table, grid_id, grid_geom,
+                       resultTable_schema = "public", resultTable_name
+                       )
+
+  {
+  
   #creates a table intersecting the input data with aggregation cells
-  #erstellt eine Tabelle die die ?berschneiden Fl?chen von Landuse mit den Fl?chen der TVZ je TVZ ausgibt
+  #erstellt eine Tabelle die die überschneiden Flächen von Landuse mit den Flächen der TVZ je TVZ ausgibt
+  
   interSection <- dbGetQuery(con, sprintf(
-    "DROP TABLE IF EXISTS %s;
-      CREATE TABLE %s (gid serial PRIMARY KEY, aggr_unit integer, use varchar);
-      ALTER TABLE %s ADD COLUMN geom geometry (MultiPolygon, 25833);
+    
+    "DROP TABLE IF EXISTS %s_intersec;
+  
+    CREATE TABLE %s_intersec (
+      gid serial PRIMARY KEY,
+      aggr_unit integer,
+      use varchar(20));
+    
+    ALTER TABLE %s_intersec ADD COLUMN geom geometry (MultiPolygon, 25833);
 
-      INSERT INTO %s (
-      SELECT row_number() over (order by 1) as gid, b.%s::numeric, a.%s, ST_Multi(ST_Intersection(a.geom, b.geom))::geometry(MultiPolygon, 25833) as geom
-      FROM %s.%s a, %s.%s b WHERE ST_Intersects(a.geom, b.geom));
-      ", intersection, intersection, intersection, intersection, aggr_id, category, schema, table, aggr_schema, aggregation))
+    INSERT INTO %s_intersec (
+      SELECT 
+        row_number() over (order by 1) as gid,
+        b.%s::numeric as aggr_unit,
+        a.%s as use,
+        ST_Multi(ST_Intersection(a.%s, b.%s))::geometry(MultiPolygon, 25833) as geom
+          FROM %s.%s AS a,
+               %s.%s AS b 
+      WHERE ST_Intersects(a.%s, b.%s) AND ST_isvalid(%s) = TRUE);
+      ",
+    resultTable_name,
+    resultTable_name,
+    resultTable_name,
+    resultTable_name,
+    grid_id,
+    ex_table_id,
+    ex_table_geom, grid_geom,
+    ex_table_schema, ex_table,
+    grid_schema, grid_table,
+    ex_table_geom, grid_geom, ex_table_geom
+    ))
 
+  
   #calculates the part of area per aggregation unit 
   #Berechnet den Fl?chenanteil pro landuse-Kategorie in jeder TVZ
-    ratio <- dbGetQuery(con, sprintf(
-      "DROP TABLE IF EXISTS %s.%s;
-      CREATE TABLE %s.%s (gid serial PRIMARY KEY, aggr_unit varchar, category varchar, area numeric, ratio numeric); 
-      INSERT INTO %s.%s (
-      SELECT row_number() over (order by 1) as gid, a.aggr_unit, a.use, round(sum(ST_AREA(a.geom))::numeric, 2) as area, round((100/ (ST_AREA(b.geom))*sum(ST_AREA(a.geom)))::numeric,2) as area_ratio 
-      FROM public.%s as a LEFT JOIN %s.%s as b ON a.aggr_unit = b.%s::numeric
-      GROUP BY a.aggr_unit, a.use, b.geom
-      ORDER BY a.aggr_unit);
-      ", out_schema, output, out_schema, output, out_schema, output, intersection, aggr_schema, aggregation, aggr_id)) 
-    delete <- dbGetQuery(con, "DROP TABLE intersection")
-    ratio
+   
+  ratio <- dbGetQuery(con, sprintf(
     
-} 
+    "DROP TABLE IF EXISTS %s.%s;
+
+    SELECT * INTO %s.%s FROM(
+      
+      SELECT 
+        row_number() over (order by 1) as gid,
+        a.aggr_unit,
+        a.use,
+        round(sum(ST_AREA(a.geom))::numeric, 2) as area,
+        round((100/ (ST_AREA(b.%s))*sum(ST_AREA(a.geom)))::numeric,2) as area_ratio,
+        b.%s
+          FROM %s_intersec as a 
+            LEFT JOIN %s.%s as b 
+              ON a.aggr_unit = b.%s::numeric
+      GROUP BY  a.aggr_unit,
+                a.use,
+                b.%s
+      ORDER BY a.aggr_unit)
+    ) as foo
+      ;"
+      ,
+      
+      resultTable_schema, resultTable_name,
+      resultTable_schema, resultTable_name,
+      grid_geom,
+      grid_geom,
+      resultTable_name,
+      grid_schema, grid_table,
+      grid_id,
+      grid_geom
+    )) 
+    
+  #delete <- dbGetQuery(con, sprintf("DROP TABLE %s_intersec;", resultTable_name))
+  
+    
+ } 
 
 
+calcAreaRatio( con, 
+               ex_table_schema = "urmo", ex_table= "fls", ex_table_id = "os_id", ex_table_geom = "the_geom",
+               grid_schema = "grids", grid_table = "fish_4000", grid_id = "gid", grid_geom = "geom",
+               resultTable_schema = "public", resultTable_name = "a_test_landuseRatio"
+               )
+
+
+
+
+
+
+ratio <- function( con,
+                   resultTable_schema, resultTable_name,
+                   grid_geom, grid_schema, grid_table, grid_id
+)
+  
+{
+
+ratio(con,
+      "public", resultTable_name,
+      grid_geom, grid_schema, grid_table, grid_id)
 
 #----------------------------------------------------------------------------------------------------------------------
 
 #Usage:
 
-#areaRatio(con, "public", "landuse", "landuse", "public", "tvz", "vbz_no", "public", "arearatio")
+#calcAreaRatio(con, "public", "landuse", "landuse", "public", "tvz", "vbz_no", "public", "arearatio")
 
-#areaRatio(con, table = "landuse", category = "landuse", aggregation = "vkz", aggr_id = "visum_no", output = "arearatio")
+#calcAreaRatio(con, ex_table = "landuse", ex_table_id = "landuse", grid_table = "vkz", grid_id = "visum_no", resultTable_name = "arearatio")
 
-#areaRatio(con, "public", table = "landuse", category = "landuse",  aggregation = "rbs_od_blk_2015_mitte", aggr_id = "blk", output = "arearatio")
+#calcAreaRatio(con, "public", ex_table = "landuse", ex_table_id = "landuse",  grid_table = "rbs_od_blk_2015_mitte", grid_id = "blk", resultTable_name = "arearatio")
 
-
+### testcase: 
 
 # #-----------------------------------------------------------------------------------------------------------------------
 # 
@@ -96,14 +175,14 @@ areaRatio <- function(con, schema = "public", table, category, aggr_schema = "pu
 #CREATE TABLE intersection (gid serial PRIMARY KEY, aggr_unit integer, use varchar);
 #ALTER TABLE intersection ADD COLUMN geom geometry (MultiPolygon, 25833);
 
-#INSERT INTO intersection (SELECT row_number() over (order by 1) as gid, b.aggr_id, a.category, ST_Multi(ST_Intersection(a.geom, b.geom))::geometry(MultiPolygon, 25833) as geom
-#FROM schema.table a, aggr_schema.aggregation b WHERE ST_Intersects(a.geom, b.geom));
+#INSERT INTO intersection (SELECT row_number() over (order by 1) as gid, b.grid_id, a.ex_table_id, ST_Multi(ST_Intersection(a.geom, b.geom))::geometry(MultiPolygon, 25833) as geom
+#FROM ex_table_schema.ex_table a, grid_schema.grid_table b WHERE ST_Intersects(a.geom, b.geom));
 
-#DROP TABLE IF EXISTS out_schema.output;
-#CREATE TABLE out_schema.output (gid serial PRIMARY KEY, aggr_unit varchar, category varchar, area numeric, ratio numeric); 
-#INSERT INTO out_schema.output (
+#DROP TABLE IF EXISTS resultTable_schema.resultTable_name;
+#CREATE TABLE resultTable_schema.resultTable_name (gid serial PRIMARY KEY, aggr_unit varchar, category varchar, area numeric, ratio numeric); 
+#INSERT INTO resultTable_schema.resultTable_name (
 #SELECT row_number() over (order by 1) as gid, a.aggr_unit, a.use, round(sum(ST_AREA(a.geom))::numeric, 2) as area, round((100/ (ST_AREA(b.geom))*sum(ST_AREA(a.geom)))::numeric,2) as area_ratio 
-#FROM public.intersection as a LEFT JOIN aggr_schema.aggregation as b ON a.aggr_unit = b.aggr_id 
+#FROM public.intersection as a LEFT JOIN grid_schema.grid_table as b ON a.aggr_unit = b.grid_id 
 #GROUP BY a.aggr_unit, a.use, b.geom
 #ORDER BY a.aggr_unit;
 
