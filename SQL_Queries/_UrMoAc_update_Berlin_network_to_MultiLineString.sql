919911803f14fe588b6aef57ceca0c8a5effb555
Kienast <Manuel.Kienast@dlr.de>
2016-11-24 11:26:32 +0100

    -- formatting

--- SQL_Queries/_UrMoAc_update_Berlin_network_to_MultiLineString.sql
+++ SQL_Queries/_UrMoAc_update_Berlin_network_to_MultiLineString.sql
@@ -1,4 +1,37 @@
-﻿----  Preparing the osm.berlin_network for usage with the UrMoAc
+﻿----  Preparing the NEW osm.new_berlin_network for usage with the UrMoAc
+---- same steps as below, copying the network to public with geometry as the geom
+DROP TABLE IF EXISTS public.Berlin_new_strassen;
+
+SELECT * INTO public.Berlin_new_strassen FROM
+    (
+      SELECT *
+          FROM osm.berlin_new_network
+    ) as foo;
+
+ALTER TABLE berlin_new_strassen
+	ADD COLUMN gid serial PRIMARY KEY;
+
+ALTER TABLE berlin_new_strassen
+	RENAME COLUMN shape TO the_geom;
+
+CREATE INDEX berlin_new_strassen_gix
+      ON berlin_new_strassen
+      USING GIST (the_geom);
+
+---
+ALTER TABLE public.Berlin_new_strassen ADD PRIMARY KEY (id);
+ALTER TABLE public.Berlin_new_strassen DROP CONSTRAINT berlin_new_strassen_pkey;
+
+ALTER Table public.Berlin_new_strassen
+	ADD COLUMN the_geom geometry (MultiLineString, 4326);
+
+ALTER TABLE public.Berlin_new_strassen
+	DROP shape;
+
+
+
+
+----  Preparing the osm.berlin_network for usage with the UrMoAc
 ---------- Copy the table to another one, maybe urmoac doesnt like computation of the same tables as net and to layer
 DROP TABLE IF EXISTS public.Berlin_strassen_test;
 
