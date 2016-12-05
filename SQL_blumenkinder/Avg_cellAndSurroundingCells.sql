c4d173bd59bbdf1408e75ad072bc0099e8281bbd
Kienast <Manuel.Kienast@dlr.de>
2016-12-05 13:02:13 +0100

    --  calc the avg of attribute in self and adjacent cells

--- SQL_blumenkinder/Avg_cellAndSurroundingCells.sql
+++ SQL_blumenkinder/Avg_cellAndSurroundingCells.sql
@@ -1,15 +1,6 @@
-﻿query builder:
+alter table rentbln_18m_dl_da_rc drop column avg_green_sourround;
 
-old_ColName: a.bid
-
-copyFromTable_name aka from:  the subselect below:
-
-copy from table id: .aid
-
-
-
-
-
+-- testing for difference in touches and intersects
 select  a.tvz_id as aid, avg(b.tvz_id::int) as bid, foo.cid
 from urmo.tvz as a
 JOIN urmo.tvz as b
@@ -34,13 +25,41 @@ ON ST_intersects((a.the_geom), b.the_geom)
 GROUP BY a.tvz_id
 ORDER BY a.tvz_id;
 
+ALTER TABLE spchar_green_tvz ALTER COLUMN geom TYPE geometry(MultiPolygon, 25833) USING ST_SetSRID(geom, 25833);
 
 select  a.tvz_id as aid, b.tvz_id::int as bid
-from urmo.tvz as a
-JOIN urmo.tvz as b
-ON ST_intersects((a.the_geom), b.the_geom)
+from spchar_green_tvz as a
+JOIN spchar_green_tvz as b
+ON ST_intersects((a.geom), b.geom)
 ORDER BY a.tvz_id;
 
+select  a.tvz_id as aid, avg(b.green) as bid
+from spchar_green_tvz as a
+JOIN spchar_green_tvz as b
+ON ST_intersects((a.geom), b.geom)
+GROUP bY a.tvz_id
+ORDER BY a.tvz_id;
 
+--- writing the query to update existing table sppachr_green_tvz
+ALTER TABLE spchar_green_tvz ADD COLUMN green_tvz_surround NUMERIC;
+WITH green_tvz AS (
+select  a.tvz_id as aid, avg(b.green) as bid
+from spchar_green_tvz as a
+JOIN spchar_green_tvz as b
+ON ST_intersects((a.geom), b.geom)
+GROUP bY a.tvz_id
+ORDER BY a.tvz_id)
+UPDATE spchar_green_tvz set green_tvz_surround = bid FROM green_tvz AS g WHERE tvz_id = g.aid;
+
+--- same for rent_blaaa writing the query to update existing table sppachr_green_tvz
+ALTER TABLE spchar_green_tvz ADD COLUMN green_tvz_surround NUMERIC;
+WITH green_tvz AS (
+select  a.tvz_id as aid, avg(b.green) as bid
+from spchar_green_tvz as a
+JOIN spchar_green_tvz as b
+ON ST_intersects((a.geom), b.geom)
+GROUP bY a.tvz_id
+ORDER BY a.tvz_id)
+UPDATE spchar_green_tvz set green_tvz_surround = bid FROM green_tvz AS g WHERE tvz_id = g.aid;
 
 SELECT * INTO public.tvz_test FROM urmo.tvz;
